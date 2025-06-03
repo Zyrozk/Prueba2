ab6b1776199088a2b859982c0fdfa8d2a4ccb2c9
Zyrozk <carofer1239@gmail.com>
2025-06-03 10:35:30 -0400

    Inicio del primer commit
    Se trabajará con js

---
+++ assets/js/script3.js
@@ -0,0 +1,14 @@
+let personas = []
+    let eTelefono = document.getElementById("telefono")
+    let vTelefono = eTelefono.value
+
+
+
+
+
+
+
+
+
+//telefono debe tener exactamente 8 digitos, no es obligatoria
+//contrasenia debe tener mas de 5 caracteres, es obligatoria
