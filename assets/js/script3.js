7638c3fa76792ca70911094fc30e712fba749b3d
Zyrozk <carofer1239@gmail.com>
2025-06-03 11:16:56 -0400

    Avance del js

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,5 +1,5 @@
 let personas = []
-    function = validar()
+    function datos(validar){
         let eTelefono = document.getElementById("telefono")
         let vTelefono = eTelefono.value
         let eErrorTelefono = document.getElementById("errorTelefono")
@@ -10,14 +10,22 @@ let personas = []
         let eErrorContrasenia = document.getElementById("errorContraseña")
         console.log(eContrasenia)
 
-        if(eTelefono.length => 8){
+        if(eTelefono.length > 8){
             alert("Debes ingresar al menos 8 digitos")
             eErrorTelefono.innerText = ""
             eErrorTelefono.innerHTML = ""
-    
+        }
         else(eTelefono)
-}
+            
 
+        if(eContrasenia.length > 6){
+            alert("Debe ingresar más de 5 caracteres")
+            eErrorContrasenia.innerText = ""
+            eErrorContrasenia.innerHTML = ""
+        }
+        else(eContrasenia)
+
+}
 
 
 
